db76d7597c5d82c65da2d82ca3e38a10d0a403cc
Axel <54435884+axdra@users.noreply.github.com>
2020-12-13 13:33:58 +0100

    Update FS2020EnableTouchInstruments.py

--- FS2020EnableTouchInstruments.py
+++ FS2020EnableTouchInstruments.py
@@ -4,20 +4,27 @@ import time
 import keyboard
 import pyautogui
 import sys
-leftKey = win32api.GetKeyState(0x01)  # Left button down = 0 or 1. Button up = -127 or -128
-rightKey = win32api.GetKeyState(0x02)  # Right button down = 0 or 1. Button up = -127 or -128
+leftKey = win32api.GetKeyState(0x01)  
+rightKey = win32api.GetKeyState(0x02) 
 readTouch = False
 running = True
-
+safeSpace = False
+#Set p1 as top left cord and p2 as bottom right cord [x,y]
+p1 = [100,100]
+p2 = [400,400]
 def toggle():
     global readTouch
     readTouch = not readTouch
 def exitapp():
     global running
     running = False
+def toggleSafeSpace():
+    global safeSpace
+    safeSpace = not safeSpace
 
 keyboard.add_hotkey('ctrl+f4', toggle)
 keyboard.add_hotkey('ctrl+f5', exitapp)
+keyboard.add_hotkey('ctrl+f6', toggleSafeSpace)
 
 
 while True:
@@ -33,14 +40,17 @@ while True:
          
             if leftRead < 0:
                 point = win32gui.GetCursorPos()
+                
                 #print(point,"l")
-                pyautogui.click(button='left', x=point[0], y=point[1]) 
+                if(not safeSpace or (point[0] >= p1[0] and point[0] <= p2[0] and point[1] >= p1[1] and point[1] <= p2[1])):
+                    print("yee")
+                    pyautogui.click(button='left', x=point[0]+1, y=point[1]+1) 
         if rightRead != rightKey: 
             rightKey = rightRead
             if rightRead < 0:
                 point = win32gui.GetCursorPos()
                 #print(point,"r")
-                pyautogui.mouseDown(button='right', x=point[0], y=point[1]) 
+                pyautogui.mouseDown(button='right', x=point[0]+1, y=point[1]+1) 
 
 
     time.sleep(0.001)
